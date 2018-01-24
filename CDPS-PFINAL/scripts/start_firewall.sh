db89f8b02f45b93c6711411e829bc44720c06592
alonso <ramon.alonsogarcia@gmail.com>
2018-01-24 23:47:31 +0100

    Fix location of fw.fw -> ./scripts/fw.fw

--- CDPS-PFINAL/scripts/start_firewall.sh
+++ CDPS-PFINAL/scripts/start_firewall.sh
@@ -5,7 +5,7 @@ sudo lxc-attach --clear-env -n fw -- uname -a
 echo '------------------------------------------------------------'
 echo 'Starting firewall...'
 
-sudo cp ./fw.fw /var/lib/lxc/fw/rootfs/etc
+sudo cp ./scripts/fw.fw /var/lib/lxc/fw/rootfs/root
 
 sudo lxc-attach --clear-env -n fw -- bash -c "cd /root; ./fw.fw"
 echo 'OK'
